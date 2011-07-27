9d4573687c536d7e0bcd4500eef6ad79bd1d9ffb
Sunng <classicning@gmail.com>
2011-07-27 16:32:36 +0800

    add init subtask

--- src/leiningen/control.clj
+++ src/leiningen/control.clj
@@ -1,23 +1,52 @@
 (ns leiningen.control
-  (:use [leiningen.compile :only (eval-in-project)])
-  (:use [control.core :only (do-begin)]))
+  (:use [control.core :only (do-begin)]
+           [leiningen.help :only (help-for)]
+           [clojure.java.io :only [file]]))
 
 (defn- get-config [project key]
   (get-in project [:control key]))
 
 (defn- load-control-file [project]
-  (try (binding [*ns* (the-ns 'control.core)]
-            (load-file
-              (if-let [control-file-name (get-config project :control-file)]
-                control-file-name "./control.clj")))
+  (try 
+    (binding [*ns* (the-ns 'control.core)]
+      (load-file
+        (if-let [control-file-name (get-config project :control-file)]
+          control-file-name "./control.clj")))
   (catch java.io.FileNotFoundException _)))
 
-(defn control
-  "run user-defined clojure-control tasks"
-  {}
-  ([project]
-    (println "Usage: lein control cluster task [args...]"))
-  ([project & args]
+(defn- run-control [project args]
+  (do
     (load-control-file project)
     (do-begin args)))
+
+(defn init
+  "Initialize clojure-control, create a sample control file in project home"
+  [project & args]
+  (let [control-file (file "." "control.clj")]
+    (if-not (.exists control-file)
+      (spit control-file 
+        (str 
+          "(defcluster :default-cluster\n"
+          "  :clients [\n"
+          "    {:host \"localhost\" :user \"root\"}\n"
+          "  ])\n"
+          "\n"
+          "(deftask :date \"Get date\""
+          "  []\n"
+          "  (ssh \"date\"))\n")))))
+
+(defn run
+  "Run user-defined clojure-control tasks against certain cluster"
+  [project & args]
+  (run-control project args))
+
+(defn control
+  "Leiningen plugin for Clojure-Control"
+  {:help-arglists '([cluster task args...])
+   :subtasks [#'init #'run]}
+  ([project subtask & args]
+    (case subtask
+      "init" (apply init project args)
+      "run" (apply run project args))))
+
 
