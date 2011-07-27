2bffd0bf1440bb16e8a81aab06979c05349a13df
Sun Ning <classicning@gmail.com>
2011-07-27 21:06:57 +0800

    add to clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject lein-control "0.1.0"
   :description "leiningen clojure-control plugin"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                            [control "0.2.0"]])
+                            [control "0.2.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
