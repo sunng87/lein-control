aba7891375c91dddafdce892b243a712f4f7ddfa
Sun Ning <classicning@gmail.com>
2011-07-29 20:22:38 +0800

    update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject lein-control "0.1.0"
   :description "leiningen clojure-control plugin"
+  :author "Sun Ning <classicning@gmail.com>"
   :dependencies [[org.clojure/clojure "1.2.1"]
                             [control "0.2.0"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
