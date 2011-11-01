1f5cadd78bf9519ec3496bdbadc742e3ee4f0275
Sun Ning <classicning@gmail.com>
2011-11-01 22:24:20 +0800

    0.2.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-control "0.2.1-SNAPSHOT"
+(defproject lein-control "0.2.1"
   :description "leiningen clojure-control plugin"
   :author "Sun Ning <classicning@gmail.com>"
   :url "https://github.com/sunng87/lein-control"
